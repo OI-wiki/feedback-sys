2b1c868c95a896168837e9e4c6bdcc35f593ec40
mgt <mgt@oi-wiki.org>
2024-04-22 22:45:07 +0800

    Add Vite SVG logo and counter functionality

--- example/docs/snippet_test.md
+++
@@ -1,3 +0,0 @@
-##### don't waste time on polyfilling pythonic features.
-
-$$ a^2 + b^2 = c^2 $$
--- gatsby-theme-oi-wiki/src/lib/trailingSlash.ts
+++
@@ -1,3 +0,0 @@
-export default function trimTrailingSlash(str?: string): string {
-  return str?.replace(/\/$/, '') ?? ''
-}
