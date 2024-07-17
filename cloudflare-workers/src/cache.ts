6cacea8cb085b2ffd898743eeba06e2e73b1230e
shaokeyibb <shaokeyibb@126.com>
2024-07-17 19:19:08 +0800

    refactor: disable cache (for now)

--- cloudflare-workers/src/cache.ts
+++ cloudflare-workers/src/cache.ts
@@ -11,13 +11,14 @@ export async function purgeAllCommentCache(env: Env, cache: Cache, baseUrl: stri
 }
 
 export async function purgeCommentCache(env: Env, cache: Cache, baseUrl: string, path: string) {
-	await cache.delete(`${baseUrl}/comment${path}`);
+	// await cache.delete(`${baseUrl}/comment${path}`);
 }
 
 export async function putCommentCache(env: Env, cache: Cache, baseUrl: string, path: string, resp: Response): Promise<void> {
-	await cache.put(`${baseUrl}/comment${path}`, resp);
+	// await cache.put(`${baseUrl}/comment${path}`, resp);
 }
 
 export async function matchCommentCache(env: Env, cache: Cache, baseUrl: string, path: string): Promise<Response | undefined> {
-	return await cache.match(`${baseUrl}/comment${path}`);
+	// return await cache.match(`${baseUrl}/comment${path}`);
+	return undefined;
 }
