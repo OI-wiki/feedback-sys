cb653f45ae611af3b6233ce715082ac2da2534ed
shaokeyibb <shaokeyibb@126.com>
2024-07-31 21:44:50 +0800

    feat: frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import autoprefixer from "autoprefixer";
+import postcssPresetEnv from "postcss-preset-env";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "./lib/main.ts",
+      name: "Review",
+      fileName: "review",
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({}),
+        postcssPresetEnv({
+          stage: 0,
+        }),
+      ],
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8787",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
